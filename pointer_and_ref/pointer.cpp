289cad03df40184728000b3f49f08069c61dbbaa
duanyzhi <duanyzhi@outlook.com>
2019-11-30 17:23:41 +0800

    [19-1130] add pointer and ref demo

---
+++ pointer_and_ref/pointer.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+int main() {
+  //////////////////////////////////////////////////////
+  int ival = 1024;
+  int &refVal = ival;  // &表示引用，将refVal和ival值绑定在一起
+  ++ival;
+  cout << refVal << "," << &refVal << "," << ival << endl;
+  // 1025,0x7ffee3e66a2c,1025
+
+  //////////////////////////////////////////////////////
+  int ival2 = 42;
+  int *p = &ival2; // *表示p是一个内存指针,存放的是变量ival2的地址，这里是0x7ffeef7659fc
+  cout << ival2 << "," << &ival2 << "," << p << "," << *p << endl;
+  // 42,0x7ffeef7659fc,0x7ffeef7659fc,42
+  // 输出*p是42,这里*是第二个功能，即取出指针p所指向内存的值
+
+  //////////////////////////////////////////////////////
+
+  //////////////////////////////////////////////////////
+}
