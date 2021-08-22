eae9f047c38793612cf398eb43ce3b379445a036
duanyzhi <duanyzhi@outlook.com>
2021-08-22 20:55:05 +0800

    add shared ptr ele

--- std_shared_ptr/main.cpp
+++ std_shared_ptr/main.cpp
@@ -1,56 +1,24 @@
 #include <iostream>
-#include  <memory> // We need to include this for shared_ptr
+#include <memory> // We need to include this for shared_ptr
  
-// https://thispointer.com/learning-shared_ptr-part-1-usage-details/
-
 int main()
 {
-	// Creating a shared_ptr through make_shared
-	std::shared_ptr<int> p1 = std::make_shared<int>();
-	*p1 = 78;
-	std::cout << "p1 = " << *p1 << std::endl;
- 
-	// Shows the reference count
-	std::cout << "p1 Reference count = " << p1.use_count() << std::endl;
- 
-	// Second shared_ptr object will also point to same pointer internally
-	// It will make the reference count to 2.
-	std::shared_ptr<int> p2(p1);
- 
-	// Shows the reference count
-	std::cout << "p2 Reference count = " << p2.use_count() << std::endl;
-	std::cout << "p1 Reference count = " << p1.use_count() << std::endl;
- 
-	// Comparing smart pointers
-	if (p1 == p2)
-	{
-		std::cout << "p1 and p2 are pointing to same pointer\n";
-	}
- 
-	std::cout<<"Reset p1 "<<std::endl;
- 
-	p1.reset();
- 
-	// Reset the shared_ptr, in this case it will not point to any Pointer internally
-	// hence its reference count will become 0.
- 
-	std::cout << "p1 Reference Count = " << p1.use_count() << std::endl;
- 
-	// Reset the shared_ptr, in this case it will point to a new Pointer internally
-	// hence its reference count will become 1.
- 
-	p1.reset(new int(11));
- 
-	std::cout << "p1  Reference Count = " << p1.use_count() << std::endl;
- 
-	// Assigning nullptr will de-attach the associated pointer and make it to point null
-	p1 = nullptr;
- 
-	std::cout << "p1  Reference Count = " << p1.use_count() << std::endl;
- 
-	if (!p1)
-	{
-		std::cout << "p1 is NULL" << std::endl;
-	}
-	return 0;
+  // Creating a shared_ptr through make_shared
+  int element = 10;
+  std::unique_ptr<float[]> ptr = std::make_unique<float[]>(element);
+
+  // std::shared_ptr<float[]> ptr = std::make_shared<float[]>(element);
+  // auto ptr = std::make_shared<float>(0.3f);
+
+  float* value = (float*)ptr.get();  // cast float[] to float*
+  for (int i = 0; i < element; i++) {
+    *value = i;
+    value++;
+  } 
+
+  float* value_ptr = (float*)ptr.get();  // cast float[] to float*
+  for (int i = 0; i < element; i++) {
+    std::cout << "value of " << i << " is: " << *(value_ptr++) << std::endl;
+  }
+  
 }
